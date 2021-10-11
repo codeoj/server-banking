ff5cb4cd7c990eaf28b8ef9c90a65bff024686cb
Oliver <oliverjgodoy@gmail.com>
2021-10-11 14:46:01 -0300

    cpf already exists rule

--- src/index.js
+++ src/index.js
@@ -9,12 +9,17 @@ const customers = [];
 
 app.post('/singup', (request, response) => {
    const { cpf, name } = request.body
-   const id = uuid();
+   //const id = uuid();
+   const alreadyExists = customers.some((customer) => customer.cpf === cpf);
    
+   if(alreadyExists) {
+      return response.status(400).json({ error: "Customer already exists!" });
+   }
+
    customers.push({
       cpf,
       name,
-      id,
+      id: uuid(),
       statement: []
    })
 
